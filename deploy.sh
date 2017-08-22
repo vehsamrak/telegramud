a8e68308658298a9c81b314173f6c505113f1cc7
Petr Karmashev <smonkl@bk.ru>
2017-08-23 00:30:12 +0300

    Tests added to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 echo "=============================="
+echo -n "Running tests... "
+go test -v ./tests/...
+
+if [ $? -ne 0 ]; then
+    echo "failed"
+    exit
+fi
+
+echo "=============================="
 echo -n "Building... "
 go build -o build/telegramud
 echo "done"
