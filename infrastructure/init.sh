0bbc12f6d1fc0f97607c03cf6f4e59c2684f5e8b
Petr Karmashev <smonkl@bk.ru>
2017-08-24 00:25:31 +0300

    Postgresql waiting timeout to 20 seconds

--- infrastructure/init.sh
+++ infrastructure/init.sh
@@ -4,7 +4,9 @@ PGPASSWORD=postgres
 PG_HOST=localhost
 PG_USER=postgres
 PG_DATABASE=postgres
-TIMEOUT=10
+TIMEOUT=20
+
+echo "Importing data to database..."
 
 until psql -h ${PG_HOST} -p 5433 -U ${PG_USER} -f init.sql > /dev/null 2>&1 || [ ${TIMEOUT} -eq 0 ]; do
   echo "Waiting for postgres server, $((TIMEOUT--)) remaining attempts..."
