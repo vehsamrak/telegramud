fdb1c9ec1e6a108f6efaa5bde28e3898f430e835
Petr Karmashev <smonkl@bk.ru>
2017-08-24 00:22:04 +0300

    Sleeping 1 second instead of 3 for postgres

--- infrastructure/init.sh
+++ infrastructure/init.sh
@@ -8,5 +8,5 @@ TIMEOUT=10
 
 until psql -h ${PG_HOST} -p 5433 -U ${PG_USER} -f init.sql > /dev/null 2>&1 || [ ${TIMEOUT} -eq 0 ]; do
   echo "Waiting for postgres server, $((TIMEOUT--)) remaining attempts..."
-  sleep 3
+  sleep 1
 done
