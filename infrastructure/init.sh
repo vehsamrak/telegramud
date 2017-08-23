a60fda05fee4fcf1b499f765904ee5ce0585c47c
Petr Karmashev <smonkl@bk.ru>
2017-08-24 00:19:54 +0300

    Waiting for postgresql in init.sh output directed to /dev/null

--- infrastructure/init.sh
+++ infrastructure/init.sh
@@ -6,7 +6,7 @@ PG_USER=postgres
 PG_DATABASE=postgres
 TIMEOUT=10
 
-until psql -h ${PG_HOST} -p 5433 -U ${PG_USER} -f init.sql || [ ${TIMEOUT} -eq 0 ]; do
+until psql -h ${PG_HOST} -p 5433 -U ${PG_USER} -f init.sql > /dev/null 2>&1 || [ ${TIMEOUT} -eq 0 ]; do
   echo "Waiting for postgres server, $((TIMEOUT--)) remaining attempts..."
   sleep 3
 done
