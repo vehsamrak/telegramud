9398f0851cbe8c2cdccff1bc259274c122b0492b
Petr Karmashev <smonkl@bk.ru>
2017-08-25 00:47:46 +0300

    Database service

---
+++ internal/services/Database.go
@@ -0,0 +1,20 @@
+package services
+
+import "github.com/jinzhu/gorm"
+
+type Database struct {
+    connection *gorm.DB
+}
+
+func (database *Database) GetConnection() *gorm.DB {
+    if database.connection != nil {
+        return database.connection
+    }
+
+    database.connection, _ = gorm.Open(
+        "postgres",
+        "host=localhost port=5433 user=telegramud password=telegramud dbname=telegramud sslmode=disable",
+    )
+
+    return database.connection
+}
