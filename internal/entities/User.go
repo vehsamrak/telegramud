aa9753aae97d41a9cdd8671c8d51fb23a9d9450e
Petr Karmashev <smonkl@bk.ru>
2017-08-25 00:48:05 +0300

    User entity mapped to ORM

--- internal/entities/User.go
+++ internal/entities/User.go
@@ -1,6 +1,15 @@
 package entities
 
+import (
+    "github.com/jinzhu/gorm"
+    _ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
 type User struct {
-    UserName string
-    Race     string
+    UserName string `gorm:"primary_key;column:name"`
+    Race     string `gorm:"column:race"`
+}
+
+func (user *User) Save(database *gorm.DB) {
+    database.Save(user)
 }
