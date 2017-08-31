bf54310d19d1af9009179223bf0a24acdb288b16
Petr Karmashev <smonkl@bk.ru>
2017-08-31 23:52:56 +0300

    Table creating removed from init.sql

--- infrastructure/sql/init.sql
+++ infrastructure/sql/init.sql
@@ -4,14 +4,9 @@ CREATE DATABASE telegramud OWNER telegramud;
 -- changing active database
 \connect telegramud telegramud
 
--- User --
-CREATE TABLE public.users
-(
-  name VARCHAR(25) PRIMARY KEY,
-  race VARCHAR(25) DEFAULT NULL
-);
+-- process initial SQL here
 
+-- creating test database
 \connect telegramud postgres
-
 DROP DATABASE IF EXISTS telegramud_test;
 CREATE DATABASE telegramud_test WITH TEMPLATE telegramud OWNER telegramud;
