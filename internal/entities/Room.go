8b10793a65664c5a5246a4c81c65ad71e5693bb8
Petr Karmashev <smonkl@bk.ru>
2017-09-01 00:32:50 +0300

    BeforeSave callback on Rooms

--- internal/entities/Room.go
+++ internal/entities/Room.go
@@ -1,15 +1,26 @@
 package entities
 
-import "github.com/jinzhu/gorm"
+import (
+    "github.com/jinzhu/gorm"
+    "encoding/json"
+)
 
 type Room struct {
     Id string `gorm:"primary_key;column:id"`
     Name string `gorm:"column:name"`
     Description string `gorm:"column:description"`
+    CoordinatesString string `gorm:"column:coordinates"`
     Coordinates *Coordinates
     Exits []*RoomPassage
 }
 
-func (user *Room) Save(database *gorm.DB) {
-    database.Save(user)
+func (room *Room) Save(database *gorm.DB) {
+    database.Save(room)
+}
+
+func (room *Room) BeforeSave() (err error) {
+    coordinatesJson, _ := json.Marshal(room.Coordinates)
+    room.CoordinatesString = string(coordinatesJson)
+
+    return
 }
