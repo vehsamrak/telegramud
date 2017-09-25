f109e225447472db2c4d02dfd8f969eeb9f6171d
Petr Karmashev <smonkl@bk.ru>
2017-09-25 23:57:37 +0300

    Running migrations in run.sh

--- run.sh
+++ run.sh
@@ -16,6 +16,11 @@ case $1 in
     docker-compose -f docker-compose.yml -f docker-compose-local.yml up -d
 ;;
 
+'migrate')
+    echo "Running migrations ..."
+    docker-compose -f docker-compose.yml -f docker-compose-local.yml run application go run -v game.go -migrate
+;;
+
 'build')
     echo "Building containers ..."
     docker-compose -f docker-compose.yml -f docker-compose-local.yml up -d --build
