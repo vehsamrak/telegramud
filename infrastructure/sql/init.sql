96b028e0c45b4e53843ad68505cf133257c6dbcc
Petr Karmashev <smonkl@bk.ru>
2017-11-27 02:49:21 +0300

    SQL password changed and hashed

--- infrastructure/sql/init.sql
+++ infrastructure/sql/init.sql
@@ -1,4 +1,4 @@
-CREATE USER telegramud WITH password 'telegramud';
+CREATE USER telegramud WITH password '5d768075a1aaa8a64c02c1d4afe8e252';
 CREATE DATABASE telegramud OWNER telegramud;
 
 -- changing active database
