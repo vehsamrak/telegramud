87fb6e1fb1aaaf23100dcf5747e2ac220c6d0ef8
Petr Karmashev <smonkl@bk.ru>
2017-11-26 04:10:51 +0300

    Travis migrations config

--- infrastructure/entrypoint-migrate.sh
+++ infrastructure/entrypoint-migrate.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+go run infrastructure/migrations/migrate.go
 go run github.com/vehsamrak/telegramud/infrastructure/migrations/migrate.go
+go run /go/src/github.com/vehsamrak/telegramud/infrastructure/migrations/migrate.go
