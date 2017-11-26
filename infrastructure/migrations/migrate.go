1876093312bc3a41f0175628bd658b873ce886f3
Petr Karmashev <smonkl@bk.ru>
2017-11-27 01:54:19 +0300

    Migrator updated to use ubenv/sql-migrate

--- infrastructure/migrations/migrate.go
+++ infrastructure/migrations/migrate.go
@@ -4,21 +4,37 @@ import (
     "os"
     "fmt"
 
-    "github.com/vehsamrak/telegramud/internal/services"
-    "github.com/vehsamrak/telegramud/internal/entities"
+    "database/sql"
+    "github.com/rubenv/sql-migrate"
+    _ "github.com/lib/pq"
 )
 
-// Run automatic migrations on database if flag -migrate passed
-func main() {
-    database := &services.Database{}
-    databaseConnection := database.GetConnection()
-    defer databaseConnection.Close()
+const DATABASE_DRIVER = "postgres"
+const DATABASE_NAME = "telegramud"
+const DATABASE_USER = "telegramud"
+const DATABASE_HOST = "database:5432/"
+const DATABASE_DATA_SOURCE = "postgres://" + DATABASE_HOST + DATABASE_NAME + "?sslmode=disable;user=" + DATABASE_USER
+const MIGRATIONS_PATH = "infrastructure/migrations"
 
+// Run database migrations
+func main() {
     fmt.Print("Migrating database...")
 
-    databaseConnection.AutoMigrate(&entities.User{}, &entities.Room{})
+    migrations := &migrate.FileMigrationSource{Dir: MIGRATIONS_PATH}
 
-    fmt.Println("done.")
+    database, error := sql.Open(DATABASE_DRIVER, DATABASE_DATA_SOURCE)
+    defer database.Close()
+
+    if error != nil {
+        fmt.Println(error)
+    }
+
+    executedMigrationsCount, error := migrate.Exec(database, DATABASE_DRIVER, migrations, migrate.Up)
+    if error != nil {
+        fmt.Println(error)
+    }
+
+    fmt.Printf("done. Applied %d migrations!\n", executedMigrationsCount)
 
     os.Exit(0)
 }
