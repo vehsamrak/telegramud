2b941977f86ae64e3d13a6ce35ebc679e9a1930c
Petr Karmashev <smonkl@bk.ru>
2018-12-04 00:35:19 +0300

    #21 Town tavern actions

--- TownCommandHandler.go
+++ TownCommandHandler.go
@@ -1,12 +1,20 @@
 package main
 
+import "github.com/go-telegram-bot-api/telegram-bot-api"
+
 type TownCommandHandler struct {
 }
 
 func (handler *TownCommandHandler) HandleCommand(chatId int64, commandName string) *CommandResult {
 	return &CommandResult{
 		Output: &Output{
-			Text:   "Вы находитесь в городе.",
+			Text: "Вы находитесь в шумной городской таверне.",
+			ReplyMarkup: tgbotapi.NewInlineKeyboardMarkup(
+				tgbotapi.NewInlineKeyboardRow(
+					tgbotapi.NewInlineKeyboardButtonData("Заказать выпивку", "drink"),
+					tgbotapi.NewInlineKeyboardButtonData("Выйти на улицу", "exit"),
+				),
+			),
 			ChatID: chatId,
 		},
 	}
